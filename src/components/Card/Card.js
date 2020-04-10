18bc9438acec2c4d16df3edde0b8e0cee99f50cd
kevin Leonard <kevinleonard.info@gmail.com>
2020-04-10 14:07:39 +0200

    add state verso

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,71 +1,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import classnames from 'classnames'
 
 export default class Card extends Component {
 
 	static propTypes = {
-		recto: PropTypes.string.isRequired,
-		verso: PropTypes.string.isRequired,
-		visibility: PropTypes.bool.isRequired,
+		verso: PropTypes.bool.isRequired,
 	}
 
-	state = {
-		editing: false,
-	}
+   	constructor(props) 
+   	{
+      super(props);
+      this.state = { verso: false };
+   	}
 
-	handleDoubleClick = () => {
-		this.setState({ editing: true })
-	}
+   	montrerVerso(){
+		this.state = { verso: true };
+   	}
 
-	handleSave = (id, text) => {
-		if (text.length === 0) {
-			this.props.deleteTodo(id)
-		} else {
-			this.props.editTodo(id, text)
-		}
-		this.setState({ editing: false })
-	}
+   	montrerRecto(){
+		this.state = { verso: false };
+   	}
 
-	render () {
-		const { todo, completeTodo, deleteTodo } = this.props
+   	retournerCarte(){
+   		if(this.state.verso){
+   			this.montrerRecto();
+   		}
+   		else{
+   			this.montrerVerso();
+   		}
+   	}
 
-		let element
-		if (this.state.editing) {
-			element = (
-				<TodoTextInput
-					text={todo.text}
-					editing={this.state.editing}
-					onSave={(text) => this.handleSave(todo.id, text)}
-				/>
-			)
-		} else {
-			element = (
-				<div className="view">
-					<input className="toggle"
-						type="checkbox"
-						checked={todo.completed}
-						onChange={() => completeTodo(todo.id)}
-					/>
-					<label onDoubleClick={this.handleDoubleClick}>
-						{todo.text}
-					</label>
-					<button
-						type="button"
-						className="destroy"
-						onClick={() => deleteTodo(todo.id)}
-					/>
-				</div>
-			)
-		}
-
-		return (
-			<li className={classnames({
-				completed: todo.completed,
-				editing: this.state.editing,
-			})}>
-				{element}
-			</li>
-		)
-	}
 }
