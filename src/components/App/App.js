7361bb17dbeb9a4328a8788add4f92faefeb7661
kevin Leonard <kevinleonard.info@gmail.com>
2020-04-10 14:48:15 +0200

    tester recto et verso

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App">
+    <Card verso={false}/>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import './Card.css';
 
 export default class Card extends Component {
 
 	static propTypes = {
-		verso: PropTypes.bool.isRequired,
+		verso: PropTypes.bool,
 	}
 
    	constructor(props) 
@@ -13,21 +14,36 @@ export default class Card extends Component {
       this.state = { verso: false };
    	}
 
-   	montrerVerso(){
-		this.state = { verso: true };
+   	montrerVerso = (event) => {
+		this.setState = { verso: true };
    	}
 
-   	montrerRecto(){
-		this.state = { verso: false };
+   	montrerRecto = (event) => {
+		this.setState = { verso: false };
    	}
 
-   	retournerCarte(){
+   	/*retournerCarte(){
    		if(this.state.verso){
    			this.montrerRecto();
    		}
    		else{
    			this.montrerVerso();
    		}
-   	}
+   	}*/
+   	render () {
+
+	   	return(
+	   	<div>
+		   	<div>
+				<div className="recto {this.state.verso === false ? 'show' : 'hidden'}"> Texte A </div>
+				<button type="button" onClick={this.montrerVerso}>Montrer B</button>
+			</div>
+			<div>
+				<div className="verso {this.state.verso === false ? 'hidden' : 'show'}"> Texte B </div>
+				<button type="button" onClick={this.montrerRecto}>Montrer A</button>
+			</div>
+		</div>
+		)
+	}
 
 }
